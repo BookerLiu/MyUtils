2709faf240258c870d36cd5cf001f91055f921c0
Demo-Liu <44599931+Demo-Liu@users.noreply.github.com>
2019-05-31 10:15:34 +0800

    Update FileClean.java
    添加保留文件路径

--- utilevent/src/main/java/com/gwssi/FileClean.java
+++ utilevent/src/main/java/com/gwssi/FileClean.java
@@ -4,6 +4,7 @@ import com.gwssi.util.PropertiesUtil;
 import org.apache.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.io.*;
 import java.util.Date;
@@ -19,30 +20,54 @@ public class FileClean {
 
     private static Logger log = Logger.getLogger(FileClean.class);
 
+    private String[] stetUrls;
 
+    private String[] DEFAULT_ARR = new String[]{};
+
+    //0 0/1 * * * ? 每一分钟
+    //0 0 0 1/1 * ? 凌晨1点
     @Scheduled(cron = "0 0 0 1/1 * ? ")
     public void deleteFile(){
+        PropertiesUtil propUtil = new PropertiesUtil();
+        String fileUrl = propUtil.getProperty("FILE_URL");
+        String stetUrls = propUtil.getProperty("STET_URL");
 
-        String fileUrl = new PropertiesUtil().getProperty("FILE_URL");
+        if(!StringUtils.isEmpty(fileUrl)){
+            //判断保留Url是否指定多个
+            if(!StringUtils.isEmpty(stetUrls)){
+                if(stetUrls.indexOf("|") == -1){
+                    this.stetUrls = new String[]{stetUrls};
+                }else{
+                    this.stetUrls = stetUrls.split("\\|");
+                }
+            }else{
+                this.stetUrls = DEFAULT_ARR;
+            }
 
-        //判断是否指定了多个url
-        if(fileUrl.indexOf("|") == -1){
-            log.info("文件清理开始,本次指定了1个Url...");
-            String[] params = fileUrl.split("\\*");
-            checkFile(params[0], Long.parseLong(params[1]));
-        }else{
-            //循环多个指定的url
-            String[] fileUrls = fileUrl.split("\\|");
-            log.info("文件清理开始,本次指定了"+fileUrls.length+"个Url...");
-            for (int i=0;i<fileUrls.length;i++) {
-                String[] params = fileUrls[i].split("\\*");
+
+            //判断是否指定了多个url
+            if(fileUrl.indexOf("|") == -1){
+                log.info("文件清理开始,本次指定了1个Url...");
+                String[] params = fileUrl.split("\\*");
                 checkFile(params[0], Long.parseLong(params[1]));
+            }else{
+                //循环多个指定的url
+                String[] fileUrls = fileUrl.split("\\|");
+                log.info("文件清理开始,本次指定了"+fileUrls.length+"个Url...");
+                for (int i=0;i<fileUrls.length;i++) {
+                    String[] params = fileUrls[i].split("\\*");
+                    checkFile(params[0], Long.parseLong(params[1]));
+                }
             }
         }
 
     }
 
     public void checkFile(String url, long day) {
+        //判断要删除的url是否在保留url中
+        for (String stetUrl : stetUrls) {
+            if(url.equals(stetUrl))return;
+        }
         log.info("url:"+url+"开始检查超过"+ day +"天的文件...");
         File file = new File(url);
         File[] files = file.listFiles();
@@ -53,6 +78,10 @@ public class FileClean {
                 //如果是文件夹  递归
                 checkFile(file.getPath(), day);
             }else{
+                //判断要删除的文件是否在保留url中
+                for (String stetUrl : stetUrls) {
+                    if(file.getPath().equals(stetUrl))return;
+                }
                 String fileName = file.getName();
                 try {
                     //判断文件是否为catalina.out
